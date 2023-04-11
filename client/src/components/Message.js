030a2b3ab152ea2ad00400edff6404152234dfa6
CharlieHerrmann1 <crh23@seas.upenn.edu>
2023-04-11 19:50:02 -0400

    chat rooms

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -4,30 +4,38 @@ import Pawn from './Pawn';
 import './Board.css'
 import io from 'socket.io-client'
 import { useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
+import { v4 as uuidv4 } from 'uuid';
 const socket = io.connect("http://localhost:3001")
 
+
+
 function Message (props) {
-  const [room, setRoom] = useState("");
   const [message, setMessage] = useState("");
   const [messageReceived, setMessageReceived] = useState("");
+   // Generate a unique ID and store it as a cookie
+   const [userId, setUserId] = useState(() => {
+    const id = Cookies.get('userId') || uuidv4();
+    Cookies.set('userId', id);
+    return id;
+  });
+
   const joinRoom = function(){
-    if(room != ""){
-      socket.emit("join_room", room);
-    }
+    socket.emit("join_room", {userId});
   }
   const sendMessage = function(){
-    socket.emit("send_message", {message, room})
+    socket.emit("send_message", {message, userId})
   } 
   useEffect(() => {
+    socket.on("connect", () => {
+      socket.emit("join_initial", {userId});
+    });
     socket.on("receive_message", (data)=>{
       setMessageReceived(data.message)
     })
   }, [socket])
   return (
     <div>
-      <input placeholder = "Room Number..." onChange={(event) => {
-        setRoom(event.target.value)
-      }}/>
       <button onClick={joinRoom}> Join Room</button>
       <input placeholder = "Message" onChange={(event) => {
         setMessage(event.target.value)
